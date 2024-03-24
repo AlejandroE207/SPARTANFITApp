d759c3cb7a8f15c3961045f0ab22bf8d7a3c4cfa
Skafind007 <skafind57@gmail.com>
2024-03-24 14:46:58 -0500

    CREACION SCRIPTS PAGINAS Y CARPETAS IMG-CSS

---
+++ Utilities/FechaUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace SPARTANFITApp.Utilities
+{
+    public class FechaUtility
+    {
+        
+        public bool ValidarDia()
+        {
+        int year = DateTime.Now.Year;
+        int month = DateTime.Now.Month;
+        int day = DateTime.Now.Day;
+
+            if (day == 1)
+            {
+             return true;   
+            }
+            return false;
+        }
+    }
+}
