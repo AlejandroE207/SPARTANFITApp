d516df95fef7949a9f6b8e92689c179c114f05a0
Skafind007 <skafind57@gmail.com>
2024-04-04 11:55:17 -0500

    creacion emetodo encriptacion y correo contrseña

---
+++ Utilities/EncriptarContrasenaUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using SPARTANFITApp.Dto;
+
+namespace SPARTANFITApp.Utilities
+{
+    public class EncriptarContrasenaUtility
+    {
+        public static string EncriptarContrasena(PersonaDto persona)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(persona.contrasena);
+
+                // Calcular el hash MD5 de la contraseña
+                byte[] hashBytes = md5.ComputeHash(passwordBytes);
+
+                // Convertir el hash en una cadena hexadecimal
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+       
+
+    }
+}
