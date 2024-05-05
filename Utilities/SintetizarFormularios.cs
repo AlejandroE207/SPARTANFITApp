10aff3d352164b034e2ed6a1fcaacbe0c763aec5
Alejandro <alejandroriveros2007@hotmail.com>
2024-05-05 09:51:31 -0500

    correccion de error github

---
+++ Utilities/SintetizarFormularios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPARTANFITApp.Utilities
+{
+    public class SintetizarFormularios
+    {
+        public string Sintetizar(string input)
+        {
+            if (input == null) return string.Empty;
+            input = input.Replace("'", " ");
+            return input;
+        }
+    }
+}
